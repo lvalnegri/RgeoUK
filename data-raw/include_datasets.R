f3e984afa217bc1b76dc30353bf73c1282362125
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-12-20 14:10:12 +0000

    changed file name

--- data-raw/include_datasets.R
+++ data-raw/include_datasets.R
@@ -1,9 +1,8 @@
-####################################################################
-# popiData * Copy datasets from PUBLIC repo to package DATA subdir #
-####################################################################
+################################################################################################
+# popipkg * Geography UK * Copy UK geographic datasets from PUBLIC repo to package DATA subdir #
+################################################################################################
 
-# datasets related to UK geography
-fnames <- c('output_areas', 'locations', 'lookups', 'hierarchies', 'workplace_zones')
+fnames <- c('output_areas', 'locations', 'location_types', 'lookups', 'hierarchies', 'workplace_zones')
 for(fn in fnames){
     message('Processing <', fn, '>')
     message(' * reading...')
@@ -15,7 +14,11 @@ for(fn in fnames){
 }
 message('Processing <postcodes>...')
 message(' * reading...')
-postcodes <- popiFun::read_fst_idx(file.path(popiFun::geouk_path, 'postcodes'), 1, c('PCU', 'OA', 'x_lon', 'y_lat'))
+postcodes <- fst::read_fst(
+                file.path(popiFun::geouk_path, 'postcodes'),
+                columns = c('PCU', 'is_active', 'OA', 'x_lon', 'y_lat'),
+                as.data.table = TRUE
+)
 message(' * saving csv...')
 data.table::fwrite(postcodes, file.path('data-raw', 'postcodes.csv'))
 message(' * saving compressed rda...')

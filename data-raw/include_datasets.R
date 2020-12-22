de486aff5aa04e428e47a54733850d484a5bb201
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-12-22 02:12:03 +0000

    added neighbours processing

--- data-raw/include_datasets.R
+++ data-raw/include_datasets.R
@@ -12,6 +12,7 @@ for(fn in fnames){
     message(' * saving compressed rda...')
     save(list = fn, file = file.path('data', paste0(fn, '.rda')), version = 3, compress = 'gzip')
 }
+
 message('Processing <postcodes>...')
 message(' * reading...')
 postcodes <- fst::read_fst(
@@ -19,10 +20,24 @@ postcodes <- fst::read_fst(
                 columns = c('PCU', 'is_active', 'OA', 'x_lon', 'y_lat'),
                 as.data.table = TRUE
 )
-
 message(' * saving csv...')
 data.table::fwrite(postcodes, file.path('data-raw', 'postcodes.csv'))
 message(' * saving compressed rda...')
 save(list = 'postcodes', file = file.path('data', 'postcodes.rda'), version = 3, compress = 'gzip')
+
+message('Processing <neighbours>...')
+message(' * reading...')
+load('data/location_types.rda')
+neighbours <- data.table::rbindlist(lapply(
+    levels(location_types$location_type),
+    function(x) {
+        if(file.exists(file.path(popiFun::geouk_path, 'neighbours', x)))
+            fst::read_fst(file.path(popiFun::geouk_path, 'neighbours', x), as.data.table = TRUE)
+    }
+))
+message(' * saving csv...')
+data.table::fwrite(neighbours, file.path('data-raw', 'neighbours.csv'))
+message(' * saving compressed rda...')
+save(list = 'neighbours', file = 'data/neighbours.rda', version = 3, compress = 'gzip')
 
 rm(list = c(fnames, 'fn', 'fnames', 'postcodes'))

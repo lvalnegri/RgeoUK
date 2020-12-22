7b96fc79c141ee272fa43c51ff7e5f1652722bdc
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-12-22 01:31:07 +0000

    got back to assign using fn (as y was wrong)

--- data-raw/include_datasets.R
+++ data-raw/include_datasets.R
@@ -1,16 +1,16 @@
-################################################################################################
-# popipkg * Geography UK * Copy UK geographic datasets from PUBLIC repo to package DATA subdir #
-################################################################################################
+#########################################################################################################
+# dmpkg.geouk * Geography Data UK * Copy UK geographic datasets from PUBLIC repo to package DATA subdir #
+#########################################################################################################
 
 fnames <- c('output_areas', 'locations', 'location_types', 'lookups', 'hierarchies', 'workplace_zones')
 for(fn in fnames){
     message('Processing <', fn, '>')
     message(' * reading...')
-    assign(y, fst::read_fst(file.path(popiFun::geouk_path, fn), as.data.table = TRUE))
+    assign(fn, fst::read_fst(file.path(popiFun::geouk_path, fn), as.data.table = TRUE))
     message(' * saving csv...')
     data.table::fwrite(y, file.path('data-raw', paste0(fn, '.csv')))
     message(' * saving compressed rda...')
-    save(list = y, file = file.path('data', paste0(fn, '.rda')), version = 3, compress = 'gzip')
+    save(list = fn, file = file.path('data', paste0(fn, '.rda')), version = 3, compress = 'gzip')
 }
 message('Processing <postcodes>...')
 message(' * reading...')
@@ -19,7 +19,10 @@ postcodes <- fst::read_fst(
                 columns = c('PCU', 'is_active', 'OA', 'x_lon', 'y_lat'),
                 as.data.table = TRUE
 )
+
 message(' * saving csv...')
 data.table::fwrite(postcodes, file.path('data-raw', 'postcodes.csv'))
 message(' * saving compressed rda...')
 save(list = 'postcodes', file = file.path('data', 'postcodes.rda'), version = 3, compress = 'gzip')
+
+rm(list = c(fnames, 'fn', 'fnames', 'postcodes'))

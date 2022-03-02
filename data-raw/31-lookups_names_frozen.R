7798b6ca321fe9074f6f0b182c4c810793e6fd79
Luca Valnegri <l.valnegri@datamaps.co.uk>
2022-03-02 02:35:48 +0000

    changed name

---
+++ data-raw/31-lookups_names_frozen.R
@@ -0,0 +1,55 @@
+##################################################
+# UK GEOGRAPHY * 31 - Lookups+Names FROZEN TYPES #
+##################################################
+
+library('data.table')
+
+# OA > LSOA > MSOA: EW, SCO, NIE
+## lookups
+y1 <- fread(
+        './data-raw/csv/ons/OA11_LSOA11_MSOA11-EW.csv', 
+        select = c('OA11CD', 'LSOA11CD', 'MSOA11CD'), 
+        col.names = c('OA', 'LSOA', 'MSOA') 
+)
+y2 <- fread('./data-raw/csv/ons/OA11_LSOA11_MSOA11-SCO.csv')
+y3 <- fread('./data-raw/csv/ons/OA11_LSOA11-NIE.csv')
+y3[, MSOA := paste0('96', substring(SOA, 3))]
+y <- rbindlist(list( y1, y2, y3 ), use.names = FALSE)
+y <- y[order(OA)]
+fwrite(y[, .(OA, LSOA)], './data-raw/csv/lookups/OA_LSOA.csv')
+fwrite(y[, .(OA, MSOA)], './data-raw/csv/lookups/OA_MSOA.csv')
+## locations
+y1 <- unique(fread( './data-raw/csv/ons/OA11_LSOA11_MSOA11-EW.csv', select = c('LSOA11CD', 'LSOA11NM'), col.names = c('LSOA', 'LSOAn') ))
+y2 <- fread('./data-raw/csv/ons/LSOA11-SCO.csv')
+y3 <- fread('./data-raw/csv/ons/LSOA11-NIE.csv')
+y <- rbindlist(list( y1, y2, y3 ), use.names = FALSE)
+fwrite(y[order(LSOA)], './data-raw/csv/locations/LSOA.csv')
+y1 <- unique(fread( './data-raw/csv/ons/OA11_LSOA11_MSOA11-EW.csv', select = c('MSOA11CD', 'MSOA11NM'), col.names = c('MSOA', 'MSOAn') ))
+y2 <- fread('./data-raw/csv/ons/MSOA11-SCO.csv')
+y <- rbindlist(list( y1, y2, y3 ), use.names = FALSE)
+y[substr(MSOA, 1, 2) == '95', MSOA := paste0('96', substring(MSOA, 3))]
+fwrite(y[order(MSOA)], './data-raw/csv/locations/MSOA.csv')
+
+## LSOA > TTWA: UK
+y <- stats::na.omit(fread('./data-raw/csv/ons/LSOA11_TTWA11-UK.csv', na.strings = ''))
+fwrite(y[, .(LSOA = LSOA11CD, TTWA = TTWA11CD)][order(LSOA)], './data-raw/csv/lookups/LSOA_TTWA.csv')
+fwrite(y[, .(TTWA = TTWA11CD, TTWAn = TTWA11NM)][order(TTWA)], './data-raw/csv/locations/TTWA.csv')
+
+## OA > MTC: EW
+y <- fread('./data-raw/csv/ons/OA11_MTC15-EW.csv')
+fwrite(y[, .(OA = OA11CD, MTC = TCITY15CD)][order(OA)], './data-raw/csv/lookups/OA_MTC.csv')
+fwrite(y[, .(MTC = TCITY15CD, MTCn = TCITY15NM)][order(MTC)], './data-raw/csv/locations/MTC.csv')
+
+## OA > BUAS > BUA: EW
+y <- fread('./data-raw/csv/ons/OA11_BUAS11_BUA11-EW.csv', na.strings = '')
+fwrite(y[, .(OA = OA11CD, BUAS = BUASD11CD)][!is.na(BUAS)][order(BUAS)], './data-raw/csv/lookups/OA_BUAS.csv')
+fwrite(y[, .(OA = OA11CD, BUA = BUA11CD)][!is.na(BUA)][order(BUA)], './data-raw/csv/lookups/OA_BUA.csv')
+fwrite(unique(y[, .(BUAS = BUASD11CD, BUASn = BUASD11NM)])[!is.na(BUAS)][order(BUAS)], './data-raw/csv/locations/BUAS.csv')
+fwrite(unique(y[, .(BUA = BUA11CD, BUAn = BUA11NM)])[!is.na(BUA)][order(BUA)]), './data-raw/csv/locations/BUA.csv')
+
+## WPZ > MSOA: EWS, NIE
+# ???
+
+message('DONE! Cleaning...')
+rm(list = ls())
+gc()
